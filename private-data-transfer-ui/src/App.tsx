7908d7d167aa20c4fceef59103f1bc3a037545be
Andrew Richardson <andrew.richardson@kaleido.io>
2021-06-09 09:58:09 -0400

    Overhaul UI to have basic broadcast support
    Removed a few things that will come back when private data is ready. For
    now, this can actually show all received messages and send a new
    broadcast.
    
    Still to add: dynamically show new messages (instead of needing a refresh),
    show two parties side-by-side (instead of only one party).
    
    Signed-off-by: Andrew Richardson <andrew.richardson@kaleido.io>

--- private-data-transfer-ui/src/App.tsx
+++ private-data-transfer-ui/src/App.tsx
@@ -1,81 +1,93 @@
 import {
   Button,
   FormControl,
+  FormControlLabel,
   Grid,
-  InputLabel,
   makeStyles,
-  MenuItem,
   Paper,
-  Select,
+  Switch,
   TextField,
 } from '@material-ui/core';
-import CloudUploadIcon from '@material-ui/icons/CloudUpload';
-import { useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
+import { FireFly, FireFlyData, FireFlyMessage } from './firefly';
 import './App.css';
 
 function App() {
   const classes = useStyles();
-  const [recipient, setRecipient] = useState<string>('');
-  const [files, setFiles] = useState<FileList | null>(null);
+  const [messages, setMessages] = useState<FireFlyMessage[]>([]);
+  const [messageData, setMessageData] = useState<Map<string, FireFlyData>>();
+  const [loading, setLoading] = useState<boolean>(true);
+
+  const firefly = new FireFly(5001);
+
+  const loadMessages = useCallback(async () => {
+    const messages = await firefly.getMessages();
+    const messageData = new Map<string, FireFlyData>();
+    for (const message of messages) {
+      for (const data of await firefly.retrieveData(message.data)) {
+        messageData.set(data.id, data);
+      }
+    }
+    setMessageData(messageData);
+    setMessages(messages);
+    setLoading(false);
+  }, []);
+
+  const sendBroadcast = () => {
+    firefly.sendBroadcast([{
+      value: "hello",
+    }]);
+  };
+
+  useEffect(() => {
+    if (loading) {
+      loadMessages();
+    }
+  });
 
   return (
     <div className={classes.root}>
       <Grid container spacing={3}>
         <Grid item xs />
         <Grid item xs={12} md={8} xl={4}>
-          <Paper className={classes.paper} component="form">
-            <h1>Send Data</h1>
+          <Paper className={classes.paper} component="form" onSubmit={sendBroadcast}>
+            <h1>Send Message</h1>
 
-            <FormControl variant="outlined" className={classes.formControl} fullWidth={true} disabled={true}>
-              <InputLabel id="recipient-label">Recipient</InputLabel>
-              <Select
-                labelId="recipient-label"
-                value={recipient}
-                onChange={event => setRecipient(event.target.value as string)}
-                label="Recipient"
-              >
-                <MenuItem value="">&nbsp;</MenuItem>
-                <MenuItem value={1}>Recipient 1</MenuItem>
-                <MenuItem value={2}>Recipient 2</MenuItem>
-              </Select>
-            </FormControl>
+            <FormControlLabel
+              control={
+                <Switch
+                  checked={true}
+                  color="primary"
+                  onClick={() => alert('Private send is not yet supported in this sample.')}
+                />
+              }
+              label="Broadcast to all recipients"
+              className={classes.formControl}
+            />
 
             <FormControl className={classes.formControl} fullWidth={true}>
-              <TextField label="Public Description" variant="outlined" />
+              <TextField label="Message" variant="outlined" />
             </FormControl>
-
-            <FormControl className={classes.formControl}>
-              <input
-                accept="*/*"
-                id="upload-file"
-                multiple
-                hidden
-                type="file"
-                onChange={event => setFiles(event.target.files)}
-              />
-              <label htmlFor="upload-file">
-                <Button
-                  variant="contained"
-                  component="span"
-                  startIcon={<CloudUploadIcon />}
-                >
-                  Select Private Files
-                </Button>
-              </label>
-              {files && <SelectedFiles files={files} />}
-            </FormControl>
-            <br />
 
             <FormControl className={classes.formControlRight}>
               <Button
                 variant="contained"
                 color="primary"
+                type="submit"
               >
                 Submit
               </Button>
             </FormControl>
 
             <div className={classes.clearFix} />
+          </Paper>
+
+          <br />
+
+          <Paper className={classes.paper}>
+            <h1>Received Messages</h1>
+
+            <MessageList messages={messages} messageData={messageData} />
           </Paper>
         </Grid>
         <Grid item xs />
@@ -84,22 +96,23 @@ function App() {
   );
 }
 
-interface SelectedFilesOptions {
-  files: FileList | null;
+interface MessageListOptions {
+  messages: FireFlyMessage[];
+  messageData?: Map<string, FireFlyData>;
 }
 
-function SelectedFiles(options: SelectedFilesOptions) {
-  const { files } = options;
-  if (files === null) {
-    return null;
-  }
+function MessageList(options: MessageListOptions) {
+  const { messages, messageData } = options;
 
   const elements = [];
-  for (let i=0; i<files.length; i++) {
-    const filename = files.item(i)?.name;
+  for (const message of messages) {
+    const data = message.data.map(d => messageData?.get(d.id));
     elements.push(
-      <li key={filename}>{filename}</li>
-    )
+      <li key={message.id}>
+        From {message.local ? "self" : message.header.author}:&nbsp;
+        {JSON.stringify(data.map(d => d?.value))}
+      </li>
+    );
   }
   return <ul>{elements}</ul>;
 }
